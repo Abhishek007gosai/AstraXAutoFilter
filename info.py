abda19614c3738a6a13a7f68b0478847d9ddb654
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-09-23 19:42:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or F
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
 O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://yoru:ymwGEw27MVghwXeX@nexus.vxpxkyz.mongodb.net/?retryWrites=true&w=majority&appName=nexus")   # This Db Is For Other Data Store
 F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://phrolova:ablH09WZeSArasy6@blade.ldtp5g2.mongodb.net/?retryWrites=true&w=majority&appName=blade")   # This Db Is For File Data Store
-S_DB_URI = environ.get('S_DB_URI', "")   # This Db is for File Data Store When First Db Is Going To Be Full.
+S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://kaya:WLORG14XP4CZWmY7@kurumi.hag8mvx.mongodb.net/?retryWrites=true&w=majority&appName=kurumi")   # This Db is for File Data Store When First Db Is Going To Be Full.
 
 
 # Premium And Referal Settings
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
