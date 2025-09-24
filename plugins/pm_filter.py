ecbc41137f6aa147ce130b67e5d74d5c4bd60043
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-09-24 13:25:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1950,8 +1950,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-            InlineKeyboardButton('Sᴏᴜʀᴄᴇ Cᴏᴅᴇ', url="https://github.com/VJBots/VJ-FILTER-BOT")
+            InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url=GRP_LNK)
         ],[
             InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
             InlineKeyboardButton('Cʟᴏsᴇ', callback_data='close_data')
@@ -3280,4 +3279,5 @@ async def global_filters(client, message, text=False):
                 break
     else:
         return False
+
 
