8b1a5ec5bb1d02de845dbf9c26d6d19d6d24bcb1
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-09-27 21:33:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -118,10 +118,10 @@ VERIFY_SHORTLINK_API = '64d631b036df348caab852591a09288cbf5b6809'
 VERIFY_TUTORIAL = 'https://t.me/+wekKcN1tjbAxY2U1'
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
-VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
+VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', True))
 # if verify second shortner is True then fill below url and api
-VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', '')
-VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
+VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'https://shortxlinks.com/')
+VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '64d631b036df348caab852591a09288cbf5b6809')
 
 
 # Shortlink Info
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
