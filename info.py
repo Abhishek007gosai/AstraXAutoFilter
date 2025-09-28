7a674b11ea6492a89b1c1aa6a9979f07df82ef9c
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-09-28 12:02:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ reqst_channel = environ.get('REQST_CHANNEL', '-1002380726940')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 
 # This Channel Is For Index Request 
-INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', ""))
+INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 
 # This Is Your Bot Support Group Id , Here Bot Will Not Give File Because This Is Support Group.
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002510253490')
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
