4bced82889f6fd8fa9c3f450e1d20cfd3813bb1d
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-10-31 16:33:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ from Script import script
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'BladeXRobot')
+SESSION = environ.get('SESSION', 'AstraXRobot')
 API_ID = int(environ.get('API_ID', '29245477'))
 API_HASH = environ.get('API_HASH', '0abc83883262245c90ca337b7a0375c4')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
