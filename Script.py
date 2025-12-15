72b79f7a93542f1c37a2f363f2b3d2c4cb306610
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-12-15 12:11:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,7 +3,7 @@
 # Ask Doubt on telegram @KingVJ01
 
 class script(object):
-    START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {} Wᴇʟᴄᴏᴍᴇ Tᴏ Cᴏᴍᴍᴜɴɪᴛʏ Iғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴜᴘᴘᴏʀᴛ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ ʏᴏᴜ ᴄᴀɴ ᴅᴏ sᴏ ʙʏ sᴜʙsᴄʀɪʙɪɴɢ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ</blockquote>\nʜᴏᴡ ᴛᴏ ᴜsᴇ ʙᴏᴛ[<a href=https://t.me/GintokiXRobot?start=BQADAQADMA8AAtR58UUG3BBcwQK9PRYE>ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴜᴛᴏʀɪᴀʟ</a>]</b>"""
+    START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {} Wᴇʟᴄᴏᴍᴇ Tᴏ Cᴏᴍᴍᴜɴɪᴛʏ Iғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴜᴘᴘᴏʀᴛ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ ʏᴏᴜ ᴄᴀɴ ᴅᴏ sᴏ ʙʏ sᴜʙsᴄʀɪʙɪɴɢ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ</blockquote>\nʜᴏᴡ ᴛᴏ ᴜsᴇ ʙᴏᴛ[<a href=https://t.me/GintokiXRobot?start=BQADAQADMwcAAp_h-UWSjG3wNmldnhYE>ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴜᴛᴏʀɪᴀʟ</a>]</b>"""
     
     CLONE_START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {}, ᴍʏ ɴᴀᴍᴇ <a href=https://t.me/{}>{}</a></blockquote>
     
@@ -743,6 +743,7 @@ Example:- /set_caption 📕 File Name: {filename}
 
 
     
+
 
 
 
