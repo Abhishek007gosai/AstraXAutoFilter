7969d855413157ce46d82c60db18b89c1840a827
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-12-16 21:03:12 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -235,9 +235,10 @@ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ @EternalsHelplineBot</b>"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}</b>
-    <b>⚙️sɪᴢᴇ : {file_size}</b>
-    <b>» <a href=https://t.me/KafkaX_Bot?start=req_LTEwMDE0NTczMTMwMjg>ᴇᴛᴇʀɴᴀʟs</a></b>""" 
+    CAPTION = """<b>{file_name}</b>
+<b>⚙️sɪᴢᴇ: {file_size}</b>
+    
+<b>» <a href=https://t.me/KafkaX_Bot?start=req_LTEwMDE0NTczMTMwMjg>ᴇᴛᴇʀɴᴀʟs</a></b>""" 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
@@ -743,6 +744,7 @@ Example:- /set_caption 📕 File Name: {filename}
 
 
     
+
 
 
 
